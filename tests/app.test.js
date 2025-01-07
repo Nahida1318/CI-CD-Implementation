6ad480d026954760b95edd78e9e21fc95fdc74b2
Nahida1318 <bsse1318@iit.du.ac.bd>
2025-01-08 01:14:35 +0600

    First commit

---
+++ tests/app.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const { app, server } = require('../index');
+
+describe('GET /', () => {
+    // afterAll(() => {
+    //     server.close(); // Close the server after all tests
+    // });
+
+    it('should return Hello World', async () => {
+        const response = await request(app).get('/');
+        expect(response.text).toBe('Hello World!');
+        expect(response.statusCode).toBe(200);
+    });
+
+    it('should fail on incorrect path', async () => {
+        const response = await request(app).get('/wrong-path');
+        expect(response.statusCode).toBe(404); // Failing case
+    });
+});
