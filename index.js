174f8252fac1499634fc90c23ad2fdaccec10bf0
Nahida1318 <bsse1318@iit.du.ac.bd>
2025-01-08 16:28:21 +0600

    solving error again

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
 // Export both `app` and `server` for testing
 module.exports = app;
