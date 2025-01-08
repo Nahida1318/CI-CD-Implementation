bc991c6b2edf737379942d55af902101ac26f165
Nahida1318 <bsse1318@iit.du.ac.bd>
2025-01-08 16:25:57 +0600

    solving error

--- tests/app.test.js
+++ tests/app.test.js
@@ -12,8 +12,13 @@ describe('GET /', () => {
         expect(response.statusCode).toBe(200);
     });
 
-    it('should fail on incorrect path', async () => {
-        const response = await request(app).get('/wrong-path');
-        expect(response.statusCode).toBe(404); // Failing case
-    });
+    // it('should fail on incorrect path', async () => {
+    //     const response = await request(app).get('/wrong-path');
+    //     expect(response.statusCode).toBe(404); // Failing case
+    // });
+    
+      it('should fail if the response is not Hello World!', async () => {
+            const res = await request(app).get('/');
+            expect(res.text).toBe('Hello Universe!'); // Intentional failing test
+          });
 });
